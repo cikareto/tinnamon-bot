e05d40f2ebd5db746a02b021b1607b1715c63904
tinnamon <salisa_cct@hotmail.com>
2019-05-28 18:50:43 +0700

    echo bot

---
+++ functions/constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+    LINE_MESSAGING_API: 'https://api.line.me/v2/bot/message',
+    LINE_HEADER: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer EZxsCnX4WEqpx9TBTv57bu01Lq/SFQDn6q+JSS1HJf2puL6McmhYXapw9gMKz0Djh73645qZJ+vGuM8C0LO1Zg7kd3q5DaA2zWszkm1gUoBlxVG0trkQWacmYNXFosjHiRVw5lM7dmqxU2Gi8xf0KwdB04t89/1O/w1cDnyilFU=`
+    }
+}
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,28 @@
 const functions = require('firebase-functions');
+const request = require('request-promise');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.TinnamonBot = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+const Constants = require('./constants');
+
+exports.TinnamonBot = functions.https.onRequest((req, res) => {
+    if(req.body.events[0].message.type !== 'text') {
+        return;
+    }
+    reply(req.body);
 });
+
+const reply = bodyResponse => {
+    return request({
+        method: `POST`,
+        uri: `${Constants.LINE_MESSAGING_API}/reply`,
+        headers: Constants.LINE_HEADER,
+        body: JSON.stringify({
+            replyToken: bodyResponse.events[0].replyToken,
+            messages: [
+                {
+                    type: `text`,
+                    text: bodyResponse.events[0].message.text
+                }
+            ]
+        })
+    });
+};
